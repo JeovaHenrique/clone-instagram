9226dbd43d6a48c11f947493c1debd6f88b8b8f1
Jeova Henrique <jeovahenrique92@gmail.com>
2021-12-11 21:59:58 -0300

    menu

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Image, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { StyleSheet, Image, Text, View, TextInput, TouchableOpacity, Alert} from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function LoginScreen({navigation}) {
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function logar() {
+        const json = {
+            user,
+            password,
+        }
+
+        const headerOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(json),
+        }
+        const response = await fetch('https://mobile.ect.ufrn.br:3000/login', headerOptions)
+        if (response.status === 200) {
+            const token = await response.text()
+            await AsyncStorage.setItem('token', token)
+            navigation.navigate('HomeScreen')
+        }else {
+            Alert.alert(
+                'Erro',
+                'Usuário ou senha inválidos',
+            )
+        }
+    }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.loginContainer}>
-          <TextInput style={styles.input} placeholder= 'Usuário...'/>
-          <TextInput style={styles.input} placeholder= 'Senha...' secureTextEntry={true}/>
-          <TouchableOpacity style={styles.sendButton}><Text>Enviar</Text></TouchableOpacity>
+          <TextInput
+           style={styles.input}
+           placeholder='Usuário...'
+           value={user}
+           onChangeText={setUser}/>
+          <TextInput
+           style={styles.input}
+            placeholder= 'Senha...'
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={true}            
+            />
+          <TouchableOpacity 
+            style={styles.sendButton}
+            onPress={() => logar()}>
+            <Text>Enviar</Text>  
+          </TouchableOpacity>
         
       </View>
     </View>
